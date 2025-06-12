a700b6c74c465567350f5f0019e8124398a17cdf
Michelle Faure <faure.s.michelle@gmail.com>
2025-06-12 19:54:08 -0400

    fix line time

--- components/History.tsx
+++ components/History.tsx
@@ -2,9 +2,9 @@ import { poppins } from "@/app/fonts/fonts";
 import Image from "next/image";
 const History = () => {
   return (
-    <div className="max-w-[1000px] text-sm  mx-auto grid gap-y-6 gap grid-cols-2 grid-rows-5 text-grey-font my-10 ps-10 pe-2">
+    <div className="max-w-[1000px] text-sm/6  mx-auto grid gap-y-6 gap grid-cols-2 grid-rows-5 text-grey-font my-10 ps-10 pe-2">
       <div
-        className={`relative shadow-lg min-h-[200px]  col-start-1 sm:col-start-2 col-end-3 ps-10 md:ps-16 py-4 pe-4  bg-lavender-light rounded-r-lg  ${poppins.className}`}
+        className={`relative shadow-lg min-h-[200px] flex items-center py-4 col-start-1 sm:col-start-2 col-end-3 ps-10 md:ps-16 pe-4  bg-lavender-light rounded-r-lg  ${poppins.className}`}
       >
         Nos conocimos en la adolescencia, en un entorno educativo que nos hacía
         ruido. Un espacio que, aunque se decía católico, parecía olvidar los
@@ -22,7 +22,7 @@ const History = () => {
       </div>
 
       <div
-        className={`relative shadow-lg col-start-1 col-end-3 sm:col-end-2 ps-10 md:ps-4 md:pe-16 py-4 pe-4 bg-lavender-light rounded-r-lg sm:rounded-r-none sm:rounded-l-lg  ${poppins.className}`}
+        className={`relative shadow-lg min-h-[200px] flex items-center col-start-1 col-end-3 sm:col-end-2 ps-10 md:ps-4 md:pe-16 pe-4 bg-lavender-light rounded-r-lg sm:rounded-r-none sm:rounded-l-lg  ${poppins.className}`}
       >
         Quizás fue la rabia ante la discriminación, el clasismo o el machismo lo
         que nos unió al principio. Lo cierto es que ahí nació un vínculo que con
@@ -37,7 +37,7 @@ const History = () => {
         />
       </div>
       <div
-        className={`relative shadow-lg col-start-1 sm:col-start-2 col-end-3 row-start-3 ps-10 md:ps-16 py-4 pe-4 bg-lavender-light rounded-r-lg  ${poppins.className}`}
+        className={`relative shadow-lg min-h-[200px] flex items-center col-start-1 sm:col-start-2 col-end-3 row-start-3 ps-10 md:ps-16 pe-4 bg-lavender-light rounded-r-lg  ${poppins.className}`}
       >
         Sin haberlo planeado, años después elegimos la misma carrera:
         psicología. Si bien partimos en escuelas distintas, la vida nos volvió a
@@ -53,7 +53,7 @@ const History = () => {
         />
       </div>
       <div
-        className={`relative shadow-lg col-start-1 col-end-3 sm:col-end-2 row-start-4 ps-10 md:ps-4 md:pe-16 py-4 pe-4 bg-lavender-light rounded-r-lg sm:rounded-r-none sm:rounded-l-lg  ${poppins.className}`}
+        className={`relative shadow-lg min-h-[200px] flex items-center col-start-1 col-end-3 sm:col-end-2 row-start-4 ps-10 md:ps-4 md:pe-16 pe-4 bg-lavender-light rounded-r-lg sm:rounded-r-none sm:rounded-l-lg  ${poppins.className}`}
       >
         Nos fuimos reconociendo en una psicología con sentido ético, humano y
         político. Nos acercamos al feminismo, a la perspectiva comunitaria y a
@@ -68,7 +68,7 @@ const History = () => {
         />
       </div>
       <div
-        className={`relative shadow-lg col-start-1 sm:col-start-2 col-end-3 row-start-5 ps-10 md:ps-16 py-4 pe-4 bg-lavender-light rounded-r-lg  ${poppins.className}`}
+        className={`relative shadow-lg min-h-[200px] flex items-center col-start-1 sm:col-start-2 col-end-3 row-start-5 ps-10 md:ps-16 pe-4 bg-lavender-light rounded-r-lg  ${poppins.className}`}
       >
         La amistad se transformó en hermandad, y esa hermandad en proyecto. Así,
         en 2022 nació Loica: un centro psicoterapéutico que es, para nosotras,
@@ -81,7 +81,10 @@ const History = () => {
           quality={100}
           className="w-[60px] absolute md:w-[100px] -left-8 top-16 md:top-8 md:-left-14 z-20"
         />
+          <div className="h-[500px]   w-full absolute z-10 -left-1 bottom-32 border-l-4 border-l-coral "></div>
+
       </div>
+
     </div>
   );
 };
