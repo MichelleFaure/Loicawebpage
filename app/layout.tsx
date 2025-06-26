71a8bbd4aa164986a985e04481d6a0d18f6842c0
Michelle Faure <faure.s.michelle@gmail.com>
2025-06-25 20:25:12 -0400

    fix seo, add sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,33 @@ import Footercomp from "@/components/Footercomp";
 
 export const metadata: Metadata = {
   title: "Centro Loica",
-  description: "Centro de Psicología y Sexología Loica",
+  description:
+    "Psicoterapia y terapia sexológica desde una mirada humanista, feminista, con perspectiva de derechos y de género. Presencia en Chile y España",
   keywords:
-    "Psicología , Sexología, atención psicológica y sexológica, Atención online",
+    "psicología, sexología, psicoterapia, terapia sexológica, atención psicológica online, salud mental, feminismo, perspectiva de género, terapia en Chile, terapia en España, terapia internacional, cursos y talleres",
   icons: {
     icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Centro Loica",
+    description:
+      "Psicoterapia y terapia sexológica desde una mirada humanista, feminista, con perspectiva de derechos y de género. Presencia en Chile y España",
+    url: "https://www.centroloica.com",
+    siteName: "Centro Loica",
+    images: [
+      {
+        url: "https://www.centroloica.com/layout/logoLoica.png",
+        width: 1200,
+        height: 675,
+        alt: "Centro Loica Logo",
+      },
+    ],
+    locale: "es_ES",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
   },
 };
 
@@ -25,11 +47,39 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Psicoterapia y terapia sexológica desde una mirada humanista, feminista, con perspectiva de derechos y de género. Presencia en Chile y España"
+        ></meta>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Centro Loica",
+              description:
+                "Psicoterapia y terapia sexológica desde una mirada humanista, feminista, con perspectiva de derechos y de género. Presencia en Chile y España",
+              url: "https://www.centroloica.com/",
+              logo: "https://www.centroloica.com/layout/logoLoica.png",
+              contactPoint: {
+                "@type": "ContactPoint",
+                telephone: "+56 9 5772 9169",
+                contactType: "customer service",
+                areaServed: "Worldwide",
+                availableLanguage: ["Spanish"],
+              },
+              sameAs: [
+                "https://www.instagram.com/centro.loica/",
+                "https://www.linkedin.com/company/centroloica",
+              ],
+            }),
+          }}
+        />
       </head>
 
-      <body
-        className={`${cormorant.className} antialiased bg-lavender-bg`}
-      >
+      <body className={`${cormorant.className} antialiased bg-lavender-bg`}>
         <Navcomp></Navcomp>
         <main>{children}</main>
         <Footercomp></Footercomp>
