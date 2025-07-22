bc9bfe2ba3c47ea55bcf012e4d8930c02b4ed872
Michelle Faure <faure.s.michelle@gmail.com>
2025-07-21 20:26:50 -0400

    fix footer

--- components/Footercomp.tsx
+++ components/Footercomp.tsx
@@ -1,84 +1,117 @@
 "use client";
-import { Footer } from "flowbite-react";
+
 import { FaLinkedin ,FaInstagram } from "react-icons/fa";
 import Image from "next/image";
 import {poppins} from "@/app/fonts/fonts";
 import { motion } from "framer-motion";
 function Footercomp() {
   return (
-    <Footer
-      container
-      className={`${poppins.className} bg-lavender-light shadow-none`}
+    <section
+      className={`${poppins.className} bg-lavender-light text-grey-font shadow-none w-full p-16`}
     >
-      <div className="flex flex-col  flex-wrap gap-10 max-w-screen-lg mx-auto ">
-        <div className="flex gap-10 items-center justify-center">
-          <div className="">
-            <Image
-              src="/layout/navbar-logo-loica.png"
-              alt="logo Centro de Psicología y Sexología Loica"
-              width={170}
-              height={144}
-              quality={100}
-            />
-          </div>
-          <div>
-            <Footer.LinkGroup col className="text-[#374151]">
-              <motion.a
-                href="https://www.linkedin.com/company/centro-loica-psicologia-y-sexologia"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center space-x-1"
-                whileHover={{ scale: 1.05, color: "#9C9CE6" }}
-                transition={{ type: "spring", stiffness: 300 }}
-              >
-                <FaLinkedin />
-                <span>Linkedin</span>
-              </motion.a>
+      <div className="flex flex-col md:flex-row flex-wrap justify-center gap-8">
+        <div className="flex flex-col items-center gap-4 text-sm bg-soft-Lavender p-4 rounded-lg shadow-lg">
+          <Image
+            src="/layout/navbar-logo-loica.png"
+            alt="logo Centro de Psicología y Sexología Loica"
+            width={170}
+            height={144}
+            quality={100}
+          />
+          <motion.a
+            href="https://www.linkedin.com/company/centro-loica-psicologia-y-sexologia"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-1"
+            whileHover={{ scale: 1.05, color: "#9C9CE6" }}
+            transition={{ type: "spring", stiffness: 300 }}
+          >
+            <FaLinkedin />
+            <span>Linkedin</span>
+          </motion.a>
 
-              <motion.a
-                href="https://www.instagram.com/centro.loica/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center space-x-1"
-                whileHover={{ scale: 1.05, color: "#9C9CE6" }}
-                transition={{ type: "spring", stiffness: 300 }}
-              >
-                <FaInstagram />
-                <span>Instagram</span>
-              </motion.a>
-            </Footer.LinkGroup>
-          </div>
+          <motion.a
+            href="https://www.instagram.com/centro.loica/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-1"
+            whileHover={{ scale: 1.05, color: "#9C9CE6" }}
+            transition={{ type: "spring", stiffness: 300 }}
+          >
+            <FaInstagram />
+            <span>Instagram</span>
+          </motion.a>
         </div>
-        <div className="flex flex-col md:flex-row md:justify-center gap-10 mt-10 text-center md:text-start">
-          <div>
-            <Footer.Title title="Enlaces" className="text-[#374151]" />
-            <Footer.LinkGroup col className="text-[#374151]">
-              <Footer.Link className="m-0" href="/">Home</Footer.Link>
-              <Footer.Link className="m-0" href="/nosotras">Nosotras</Footer.Link>
-              <Footer.Link className="m-0" href="/servicios">Servicios</Footer.Link>
-              <Footer.Link className="m-0"  href="/cursosytalleres">
-                Cursos y Talleres
-              </Footer.Link>
-              <Footer.Link className="m-0" href="/agenda">Agenda</Footer.Link>
-            </Footer.LinkGroup>
-          </div>
-          <div>
-            <Footer.Title title="Ubicación" className="text-[#374151]" />
-            <Footer.LinkGroup col className="text-[#374151]">
-              <div> Santiago, Chile</div>
-              <div> Almería, España</div>
-            </Footer.LinkGroup>
-          </div>
-          <div>
-            <Footer.Title title="Contacto" className="text-[#374151]" />
-            <Footer.LinkGroup col className="text-[#374151]">
-              <div> loica.contactos@gmail.com</div>
-              <Footer.Link href="/comentarios">Cuentanos tu experiencia</Footer.Link>
-            </Footer.LinkGroup>
-          </div>
+        <div className="flex flex-col gap-2 text-center md:text-start">
+          <div className="font-semibold ">Enlaces</div>
+          <ul className="text-sm flex flex-col gap-2">
+            <motion.li
+              className="cursor-pointer"
+              whileHover={{ scale: 1.05, color: "#9C9CE6" }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <a href="/">Inicio</a>
+            </motion.li>
+            <motion.li
+              className="cursor-pointer"
+              whileHover={{ scale: 1.05, color: "#9C9CE6" }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <a href="/nosotras">Nosotras</a>
+            </motion.li>
+            <motion.li
+              className="cursor-pointer"
+              whileHover={{ scale: 1.05, color: "#9C9CE6" }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <a href="/servicios">Terapias</a>
+            </motion.li>
+            <motion.li
+              className="cursor-pointer"
+              whileHover={{ scale: 1.05, color: "#9C9CE6" }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <a href="/cursosytalleres">Cursos</a>
+            </motion.li>
+            <motion.li
+              className="cursor-pointer"
+              whileHover={{ scale: 1.05, color: "#9C9CE6" }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <a href="/recursos">Recursos</a>
+            </motion.li>
+            <motion.li
+              className="cursor-pointer"
+              whileHover={{ scale: 1.05, color: "#9C9CE6" }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <a href="/agenda">Agenda</a>
+            </motion.li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-2 text-center md:text-start">
+          <div className="font-semibold ">Ubicación</div>
+          <ul className="text-sm flex flex-col gap-2">
+            <li> Santiago, Chile</li>
+            <li> Almería, España</li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-2 text-center md:text-start">
+          <div className="font-semibold ">Contacto</div>
+          <ul className="text-sm flex flex-col gap-2">
+            <li> loica.contactos@gmail.com</li>
+            <motion.li
+              className="cursor-pointer"
+              whileHover={{ scale: 1.05, color: "#9C9CE6" }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              Cuentanos tu experiencia
+            </motion.li>
+          </ul>
         </div>
       </div>
-    </Footer>
+    </section>
+
   );
 
 }
